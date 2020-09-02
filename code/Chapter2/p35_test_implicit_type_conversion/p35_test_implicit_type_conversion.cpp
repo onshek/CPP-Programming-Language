38fd73026c4a2de8760270b4497342532d9e41fd
Honfung Wong <21543236+onshek@users.noreply.github.com>
2020-09-02 10:54:39 +0800

    UPT：p35_test_implicit_type_conversion and .gitignore

---
+++ code/Chapter2/p35_test_implicit_type_conversion/p35_test_implicit_type_conversion.cpp
@@ -0,0 +1,18 @@
+﻿// 用于测试运算符的自动类型转换
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+int main() {
+    float fVal = 1.0;
+    double dVal;
+    int iVal = 3;
+    unsigned long ulVal = 4;
+    dVal = iVal * ulVal;
+    cout << typeid(iVal * ulVal).name() << endl;
+    cout << typeid(dVal).name() << endl;
+    dVal = ulVal + fVal;
+    cout << typeid(ulVal + fVal).name() << endl;
+    cout << typeid(dVal).name() << endl;
+    return 0;
+}
