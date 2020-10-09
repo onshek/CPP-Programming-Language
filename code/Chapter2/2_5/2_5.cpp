ac9bb0032dccee54700dbe66f78b168b3732c0cb
Honfung Wong <21543236+onshek@users.noreply.github.com>
2020-10-09 21:39:11 +0800

    upload 2_5-3_1

---
+++ code/Chapter2/2_5/2_5.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0, sum = 0;
+    while (i <= 10) {
+        sum += i;
+        i++;
+    }
+    cout << "sum=" << sum << endl;
+    return 0;
+}
