7a7b88385ad4a69172443e76f8dddc302dda297d
Honfung Wong <21543236+onshek@users.noreply.github.com>
2020-10-10 21:28:49 +0800

    ADD: 3_1-3_5

--- code/Chapter3/3_1/3_1.cpp
+++ code/Chapter3/3_1/3_1.cpp
@@ -1,6 +1,15 @@
 ﻿#include <iostream>
 using namespace std;
 
+double power(double x, int n) {
+    double val = 1.0;
+    while (n--) {
+        val *= x;
+    }
+    return val;
+}
+
 int main() {
-    std::cout << "Hello World!\n";
+    cout << "5 to the power 2 is " << power(5, 2) << endl;
+    return 0;
 }
---
+++ code/Chapter3/3_5/3_5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double TINY_VALUE = 1e-10;
+
+double tsin(double x) {
+    double g = 0;
+    double t = x;
+    int n = 1;
+    do {
+        g += t;
+        n++;
+
+    }
+}
+
+int main() {
+    std::cout << "Hello World!\n";
+}
