8b8ef11c6342cb83bb4b4365ca6767a71242d511
Honfung Wong <21543236+onshek@users.noreply.github.com>
2020-10-11 02:33:25 +0800

    UPD: 3_5 & 3_6

--- code/Chapter3/3_5/3_5.cpp
+++ code/Chapter3/3_5/3_5.cpp
@@ -11,10 +11,23 @@ double tsin(double x) {
     do {
         g += t;
         n++;
-
-    }
+        t = -t * x * x / (2 * n - 1) / (2 * n - 2);
+    } while (fabs(t) >= TINY_VALUE);
+    return g;
 }
 
 int main() {
-    std::cout << "Hello World!\n";
+    double k, r, s;
+    cout << "r=";
+    cin >> r;
+    cout << "s=";
+    cin >> s;
+    if (r * r < s * s) {
+        k = sqrt(tsin(r) * tsin(r) + tsin(s) * tsin(s));
+    }
+    else {
+        k = tsin(r * s) / 2;
+    }
+    cout << k << endl;
+    return 0;
 }
---
+++ code/Chapter3/3_6/3_6.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+// 投骰子，计算和数，输出和数
+int rollDice() {
+    int die1 = 1 + rand() % 6;
+    int die2 = 1 + rand() % 6;
+    int sum = die1 + die2;
+    cout << "player rolled " << die1 << "+" << die2 << "=" << sum;
+    return sum;
+}
+
+enum GameStatus { WIN, LOSE, PLAYING };
+
+int main() {
+    int sum, myPoint;
+    GameStatus status;
+
+    unsigned seed;
+    cout << "Please enter an unsigned integer: ";
+    cin >> seed; // 传入随机数种子
+    srand(seed); // 将种子传递给 rand()
+
+    sum = rollDice();  // 第一轮投骰子，计算和数
+    switch (sum) {
+    case 7:
+    case 11:
+        status = WIN;
+        break;
+    case 2:
+    case 3:
+    case 12:
+        status = LOSE;
+        break;
+    default:
+        status = PLAYING;
+        myPoint = sum;
+        cout << "point is" << myPoint << endl;
+        break;
+    }
+
+    while (status == PLAYING) {
+
+    }
+}
