0335d13a24e3599bfc7029e7b4f414a220f0425d
Honfung Wong <21543236+onshek@users.noreply.github.com>
2020-10-25 20:11:42 +0800

    UPL: 4_2-4_3

---
+++ code/Chapter4/test_p105_inline_function_witin_a_class/test_p105_inline_function_witin_a_class.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Test {
+public:
+    inline void printA() {
+        cout << "func: printA" << endl;
+    }
+    void printB() { // 隐式声明内联函数
+        cout << "func: printB" << endl;
+    }
+    inline void printC();
+    void printD();
+};
+
+inline void Test::printC() {
+    cout << "func: printC" << endl;
+}
+
+inline void Test::printD() {
+    cout << "func: printD" << endl;
+}
+
+int main() {
+    Test test;
+    test.printA();
+    test.printB();
+    test.printC();
+    test.printD();
+    return 0;
+}
