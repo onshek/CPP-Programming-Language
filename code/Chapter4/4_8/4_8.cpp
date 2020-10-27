cf55a966f178c11b7bea8b9133de6b891b843706
Honfung Wong <21543236+onshek@users.noreply.github.com>
2020-10-27 20:25:00 +0800

    UPL: 4_7-4_8

---
+++ code/Chapter4/4_8/4_8.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <iostream>
+using namespace std;
+
+class ExamInfo {
+public:
+    ExamInfo(string name, char grate)
+        : name(name), mode(GRADE), grade(grate) {}
+    ExamInfo(string name, bool pass)
+        : name(name), mode(PASS), pass(pass) {}
+    ExamInfo(string name, int percent)
+        : name(name), mode(PERCENTAGE), percent(percent) {}
+    void show();
+private:
+    string name;
+    enum {
+        GRADE,
+        PASS,
+        PERCENTAGE
+    } mode;
+    union {
+        char grade;
+        bool pass;
+        int percent;
+    };
+};
+
+void ExamInfo::show() {
+    cout << name << ": ";
+    switch (mode) {
+    case GRADE:
+        cout << grade;
+        break;
+    case PASS:
+        cout << (pass ? "PASS" : "FAIL");
+        break;
+    case PERCENTAGE:
+        cout << percent;
+        break;
+    }
+    cout << endl;
+}
+
+int main() {
+    ExamInfo course1("English", 'B');
+    ExamInfo course2("Calculus", true);
+    ExamInfo course3("C++Programming", 85);
+    course1.show();
+    course2.show();
+    course3.show();
+    return 0;
+}
