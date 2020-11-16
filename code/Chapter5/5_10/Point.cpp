5101123d699e78f7fb0a8c17244f4cd17563cc25
Honfung Wong <21543236+onshek@users.noreply.github.com>
2020-11-16 23:22:52 +0800

    UPD: 5_10

--- code/Chapter5/5_10/Point.cpp
+++ code/Chapter5/5_10/Point.cpp
@@ -1,12 +1,12 @@
-class Point {
-public:
-	Point(int x = 0, int y = 0) :x(x), y(y) {}
-	Point(const Point& p);
-	~Point() { count--; }
-	int getX() const { return x; }
-	int getY() const { return y; }
-	static void showCount();
-private:
-	int x, y;
-	static int count;
-};
+#include "Point.h"
+#include <iostream>
+using namespace std;
+
+int Point::count = 0;
+Point::Point(const Point& p) :x(p.x), y(p.y) {
+	count++;
+}
+
+void Point::showCount() {
+	cout << "    Object count=" << count << endl;
+}
