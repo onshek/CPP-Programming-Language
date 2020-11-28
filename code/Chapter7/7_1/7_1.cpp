6968c14745908764a68f1f24012c0e552db65b1a
Honfung Wong <21543236+onshek@users.noreply.github.com>
2020-11-28 08:16:44 +0800

    ADD: 7_1

---
+++ code/Chapter7/7_1/7_1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ code/Chapter7/7_1/Rectangle.h
@@ -0,0 +1,4 @@
+#ifndef _RECTANGLE_H
+#define _RECTANGLE_H
+
+#endif //_RECTANGLE_H
