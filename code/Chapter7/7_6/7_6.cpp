b78a73392014c29dcc84fd2c3018a4142ac73e62
Honfung Wong <21543236+onshek@users.noreply.github.com>
2021-02-23 16:19:17 +0800

    UPD: 7_6

--- code/Chapter7/7_6/7_6.cpp
+++ code/Chapter7/7_6/7_6.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+class Base1 {
+public:
+    int var;
+    void fun() { cout << "Member of Base1" << endl; }
+};
+
+class Base2 {
+public:
+    int var;
+    void fun() { cout << "Member of Base2" << endl; }
+};
+
+class Derived : public Base1, public Base2 {
+public:
+    int var;
+    void fun() { cout << "Member of Derived" << endl; }
+};
+
+
+int main()
+{
+    Derived d;
+    Derived* p = &d;
+
+    d.var = 1;
+    d.fun();
+
+    d.Base1::var = 2;
+    d.Base1::fun();
+
+    p->Base2::var = 3;
+    p->Base2::fun();
+
     return 0;
 }
