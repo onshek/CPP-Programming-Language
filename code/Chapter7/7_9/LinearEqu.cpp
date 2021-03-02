f33f0a6d6634fbf6dd0119f8d3000106644242cf
Honfung Wong <21543236+onshek@users.noreply.github.com>
2021-03-02 23:16:39 +0800

    ADD: 7_9

---
+++ code/Chapter7/7_9/LinearEqu.cpp
@@ -0,0 +1,33 @@
+#include "LinearEqu.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+LinearEqu::LinearEqu(int size/*=2*/) :Matrix(size) {
+	sums = new double[size];
+	solution = new double[size];
+}
+
+LinearEqu::~LinearEqu() {
+	delete[] sums;
+	delete[] solution;
+}
+
+void LinearEqu::printLinearEqu() const {
+	cout << "The Line eqution is:" << endl;
+	for (int i = 0; i < getSize(); i++) {
+		for (int j = 0; j < getSize(); j++) {
+			cout << element(i, j) << " ";
+		}
+		cout << "    " << sums[i] << endl;
+	}
+}
+
+void LinearEqu::printSolution() const {
+	cout << "The Result is: " << endl;
+	for (int i = 0; i < getSize(); i++) {
+		cout << "X[" << i << "]=" << solution[i] << endl;
+	}
+}
+
+
